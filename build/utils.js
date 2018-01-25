c955b94c8e2823285daf1a41eae9e45bc09ba776
iocool <fannypanther@gmail.com>
2018-01-25 14:24:49 +0800

    修改build下utils.js文件,解决背景图不能正常显示问题

--- build/utils.js
+++ build/utils.js
@@ -47,7 +47,8 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        fallback: 'vue-style-loader'
+        fallback: 'vue-style-loader',
+        publicPath: '../../'  // 为解决背景图片不能显示的问题
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
